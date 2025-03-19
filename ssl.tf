c5740f2aab5032ba630664e0647b575bd63ce711
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2025-03-19 17:40:22 +0100

    fix: ssl policy description

--- ssl.tf
+++ ssl.tf
@@ -2,6 +2,7 @@ resource "google_compute_ssl_policy" "modern_tls_1_2" {
   count           = var.enable_ssl_policy ? 1 : 0
   name            = "modern-ssl-policy"
   profile         = "MODERN"
+  description     = "Modern SSL policy with minimum TLS version 1.2"
   min_tls_version = "TLS_1_2"
 }
 
