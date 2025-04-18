35bdd5d30d30feca58be31d0838579f3b30bcfd2
Stefano Sibilia <stefano.sibilia@sparkfabrik.com>
2025-04-18 12:47:38 +0200

    refs platform/#3571: add modern SSL policy description variable

--- variables.tf
+++ variables.tf
@@ -12,6 +12,12 @@ variable "enable_ssl_policy" {
   default     = true
 }
 
+variable "ssl_modern_policy_description" {
+  description = "Description for the SSL policy"
+  type        = string
+  default     = "Modern SSL policy with minimum TLS version 1.2"
+}
+
 ###########################
 # Logging Exclusions 
 ###########################
